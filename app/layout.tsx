f00f741b9db89db33a312e892bf23409022f1862
steff04 <91566451+Steffanoslc@users.noreply.github.com>
2023-07-08 18:33:27 -0400

    env file test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import './globals.css'
+
 export const metadata = {
   title: 'Flexibble',
-  description: 'a clone from dribbble.com',
+  description: '(a clone from dribbble.com) Showcase and discover creative work on the world\'s leading online platform for creative industries.',
 }
 
 export default function RootLayout({
@@ -10,7 +12,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        Navbar
+        <main>
+          {children}
+        </main>
+        Footer
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>Catergories</h1>
+      <h1>Posts</h1>
+      <h1>LoadMore</h1>
+    </section>
   )
 }
 
